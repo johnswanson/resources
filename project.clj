26f0447cccf43aa6d8396b320d91eaf6e1abbde0
John Swanson <john.d.swanson@gmail.com>
2015-06-24 22:24:10 -0700

    add description and URL to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.jds02006/resources "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A truly declarative RESTful API library"
+  :url "https://github.com/johnswanson/resources"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
